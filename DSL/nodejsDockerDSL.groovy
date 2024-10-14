6321561dfe270fd85ae4c1b98e2a75b8c18f8b26
farid1275 <farid27orbe@gmail.com>
2024-10-14 12:38:28 +0200

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,8 +1,8 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/Farid1275/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('Farid1275')
+        git('https://github.com/farid1275/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('farid1275')
             node / gitConfigEmail('farid27orbe@gmail.com')
         }
     }
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('Farid1275/nodejsapp')
+            repositoryName('farid1275/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
